2b634f57beba68af2e02361aacb68f1df9adc3b9
Davin Tryon <davintryon@gmail.com>
2016-07-20 20:26:35 +0100

    some cleanup

--- src/cljs/relog/components/editor.cljs
+++ src/cljs/relog/components/editor.cljs
@@ -8,16 +8,14 @@
 (def markdown (r/atom "# Some Markdown"))
 
 (defn onChange [e]
-  (let [content (.-innerHTML (r/dom-node e.target))]
-    (.log js/console e.target.value)
-    (reset! markdown e.target.value)))
-
-
+    (reset! markdown e.target.value))
 
 (defn Editor []
   (fn []
     [:div {:className "Editor grid grid-row"}
       [:div {:className "Editor_markdown grid-col-xs-6"}
-       [:textArea {:className "Editor_markdown_area" :onChange onChange} @markdown]]
+       [:textArea {:className "Editor_markdown_area"
+                   :onChange onChange
+                   :value @markdown}]]
       [:div {:className "Editor_rendered Markdown grid-col-xs-6"
              :dangerouslySetInnerHTML {:__html (js/marked @markdown)}}]]))
